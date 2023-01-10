97280880b4a07e759bde468ee2805166796ebcba
JackDilger <jack.dilger1995@gmail.com>
2023-01-10 19:21:19 +0000

    Add nav bar using class component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,11 @@
 import css from './App.module.css';
-
 import FunctionalSidebar from './components/Sidebar';
+import NavBarSimple from './components/NavBarSimple';
 
 function App() {
   return (
     <div className={css.App}>
-      <FunctionalSidebar/>
+      <NavBarSimple/>
     </div>
   );
 }
--- my-app/src/components/Sidebar.js
+++ my-app/src/components/Sidebar.js
@@ -11,4 +11,5 @@ const FunctionalSidebar = () => {
     );
 }
 
+
 export default FunctionalSidebar;
